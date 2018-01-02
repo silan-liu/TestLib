c5a66c6d2dc2b14b4d65e3a84b7e9bf4a0fb7163
silan-liu <413769707@qq.com>
2018-01-02 21:37:42 +0800

    first commit

---
+++ TestLib/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TestLib
+//
+//  Created by silan on 2018/1/2.
+//  Copyright © 2018年 summer. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ TestLib/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  TestLib
+//
+//  Created by silan on 2018/1/2.
+//  Copyright © 2018年 summer. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
